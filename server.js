c08df2c7b938c4cf9709b784b11307d238790fae
KUOKA Yusuke <ykuoka@gmail.com>
2010-10-28 09:41:02 -0700

    Webページ側をexpressに移行

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-var express = require('express');
-
-var http = require('http'), 
-		URL = require('url'),
-		fs = require('fs'),
-		io = require('./lib/socket.io-node'),
-		sys = require('sys'),
-    OAuth = require('./lib/node-oauth').OAuth;
-var twitterUrl = ""
-    var oauth = new OAuth("https://api.twitter.com/oauth/request_token",
+var express = require('express')
+    http = require('http'),
+    URL = require('url'),
+    fs = require('fs'),
+    socketio = require('./lib/socket.io-node'),
+    sys = require('sys'),
+    OAuth = require('./lib/node-oauth').OAuth,
+    RedisStore = require('connect-redis'),
+    twitterUrl = "",
+    oauth = new OAuth("https://api.twitter.com/oauth/request_token",
 			  "https://api.twitter.com/oauth/access_token",
 			  "tzQ3s6FopvC5NuosF1QqGw",
 			  "Tjl302eKWS675Kr8AScUkSzVWMJJqYMazQLztuj0eE",
@@ -15,118 +15,108 @@ var twitterUrl = ""
 			  "http://localhost:8080/oauth_authorized",
 			  "HMAC-SHA1");
     
-var HOST = '192.168.100.165:8080';
-		
-server = http.createServer(function(req, res){
-	// your normal server code
-	var url = URL.parse(req.url);
-	var host = url.host;
-	var path = url.pathname;
-	switch (path){
-		case '/':
-			res.writeHead(200, {'Content-Type': 'text/html'});
-			res.write('<h1>Welcome. Try the <a href="/chat.html">chat</a> example.</h1>');
-			res.end();
-			break;
-			
-		case '/json.js':
-		case '/chat.html':
-		    if (path === '/chat.html' && !session.token_secret) {
-			requestAuthorization(req, res);
-			break;
-		    }
-			fs.readFile(__dirname + path, function(err, data){
-				if (err) return send404(res);
-				res.writeHead(200, {'Content-Type': path == 'json.js' ? 'text/javascript' : 'text/html'})
-				res.write(data, 'utf8');
-				res.end();
-			});
-			console.log(sys.inspect(session));
-			getUser();
-			break;
-	case '/oauth_authorized':
-	    authorized(req, res);
-	    break;
-		default: send404(res);
-	}
-}),
+var app = express.createServer();
 
-send404 = function(res){
-    res.writeHead(404);
-    res.write('404');
-    res.end();
-};
+/* configurations */
+app.configure(function() {
+  app.set('views', __dirname + '/views');
+  app.use(express.bodyDecoder());
+  app.use(express.cookieDecoder());
+  app.use(express.errorHandler({dumpException: true, showStack: true}));
+  app.use(express.staticProvider(__dirname + '/public'));
+  app.use(express.session(/* {store: new RedisStore} */));
+});
 
-function redirect(res, url) {
-    res.writeHead(302, {'Location': url});
-    res.end();
-}
+app.error(function(err, req, res, next) {
+  sys.log(err);
+  sys.log(req);
+  sys.log(res);
+  res.send(err.message, 500);
+});
 
+app.get('/req', function(req, res) {
+  res.send(sys.inspect(req));
+});
+
+app.get('/',function(req, res){
+  res.send('<h1>Welcome. Try the <a href="/chat">chat</a> example.</h1>');
+});
+
+app.get('/chat', function(req, res) {
+  if (!req.session.token_secret) {
+    requestAuthorization(req, res);
+  } else {
+    var user = getUser(req);
+    res.contentType('.html');
+    res.render('chat.ejs');
+  }
+});
+
+app.get('/html', function(req, res) {
+  res.contentType('.html');
+  res.render('chat.ejs');
+});
+
+app.get('/oauth_authorized', function(req, res) {
+  authorized(req, res);
+});
+
+/** 認証がまだならtwitterにリダイレクト */
 function requestAuthorization(req, res) {
-    oauth._authorize_callback = 'http://' + HOST + '/oauth_authorized';
+    var host = req.headers.host;
+    oauth._authorize_callback = 'http://' + host + '/oauth_authorized';
     oauth.getOAuthRequestToken(function(error, token, token_secret, results) {
 	    console.log(sys.inspect(results));
-	    if (onError(res, error)) {
-		return;
+	    if (error) {
+		throw new Error(sys.inspect(error));
 	    }
-	    session.token = token;
-	    session.token_secret = token_secret;
-	    redirect(res, 'https://api.twitter.com/oauth/authorize?oauth_token=' + token);
+	    req.session.token = token;
+	    req.session.token_secret = token_secret;
+	    res.redirect('https://api.twitter.com/oauth/authorize?oauth_token=' + token);
 	});
 }
 
-var session = {};
-
+/** ユーザの承認後、Twitterから戻ってきたとき */
 function authorized(req, res) {
-    var token = session.token;
-    var token_secret = session.token_secret;
-    var query = URL.parse(req.url, true).query;
-    var verifier = query.oauth_verifier;
+    var token = req.session.token;
+    var token_secret = req.session.token_secret;
+    var verifier = req.query.oauth_verifier;
+    var host = req.headers.host;
 
     oauth.getOAuthAccessToken(token, token_secret, verifier, function(error, access_token, access_token_secret, results2) {
-	    console.log(sys.inspect(arguments));
-	    if (onError(res, error)) {
-		return; 
+	    if (error) {
+		throw new Error(sys.inspect(error));
 	    }
-	    //	    res.writeHead(200, {'Content-Type': 'text/html'});
-	    //	    res.write('token=' + token + ',token_secret=' + token_secret + ', access_token=' + access_token + ', access_token_secret=' + access_token_secret, 'utf8');
-	    session.token = access_token;
-	    session.token_secret = access_token_secret;
-	    //	    res.end();
-	    redirect(res, 'http://' + HOST + '/chat.html');
+	    req.session.token = access_token;
+	    req.session.token_secret = access_token_secret;
+	    res.redirect('http://' + host + '/chat', 301);
 	});
 }
 
-function onError(res, error) {
-    if (error) {
-	returnText(res, sys.inspect(error));
-	return true;
-    }
-    return false;
+var connectSessions = {};
+
+/**
+ * OAuth認証しつつtwitterのverify_credentialsでユーザ情報を取得
+ */
+function getUser(req) {
+  var token = req.session.token;
+  var token_secret = req.session.token_secret;
+  var host = 'api.twitter.com';
+  var path = '/1/account/verify_credentials.json';
+
+  oauth.getProtectedResource('http://' + host + path, 'GET', token, token_secret, function(error, data, response) {
+    var user = JSON.parse(data);
+    req.session.user = user;
+    connectSessions[req.cookies['connect.sid']] = req.session;
+  });
 }
 
-function getUser() {
-    var host = 'api.twitter.com';
-    var path = '/1/account/verify_credentials.json';
-    oauth.getProtectedResource('http://' + host + path, 'GET', session.token, session.token_secret, function(error, data, response) {
-	    console.log(sys.inspect(arguments));
-	    console.log(typeof data, data);
-	    console.log(JSON.parse(data).screen_name);
-	});
-}
-
-function returnText(res, text) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(text, 'utf8');
-    res.end();
-}
-
-server.listen(8080);
+app.listen(8080);
 
 // socket.io, I choose you
 // simplest chat application evar
-var io = io.listen(server),
-		buffer = [];
+var io = socketio.listen(app),
+    buffer = [];
 		
 io.on('connection', function(client){
 	client.send({ buffer: buffer });
